02a246730da7cec900f9cf0a61027407f1d6d047
James Briggs, Sr. Devops/SRE/DBA <james.briggs@yahoo.com>
2021-09-28 21:58:24 -0700

    Create ip_forecast.sh

---
+++ ip_forecast.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Program: ip_forecast.sh
+# Purpose: show current weather for a location using an IP address, demonstrating curl and jq
+# Author: James Briggs
+# Date: 2021 02 08
+# Env: bash
+# Usage: ip_forecast.sh [IP]
+# Note:
+
+ip=$1
+
+os_linux=1
+
+if grep -q "apple" <<< "$MACHTYPE"; then
+   os_linux=0
+fi
+
+info=`curl -s -o - https://ipinfo.io/$ip`
+
+loc=`echo $info | jq -r '.loc'`
+city=`echo $info | jq -r '.city'`
+if [[ "$ip" == "" ]]; then
+   ip=`echo $info | jq -r '.ip'`
+fi
+
+echo "Weather forecast for $ip ($city):"
+
+out=`curl -s -o - https://api.darksky.net/forecast/6ce2cb95ebf7afee7f2d76afcc037fb3/$loc`
+
+today=`date +'%Y-%m-%d'`
+
+n=0
+echo $out | jq -r '.daily.data[] | [ .time, .summary ] | @tsv' |
+  while IFS=$'\t' read -r t s; do
+      if [[ "$os_linux" == 1 ]]; then
+         dt=`date -d @$t +'%Y-%m-%d'`
+      else
+         dt=`date -r $t +'%Y-%m-%d'`
+      fi
+
+      if [[ "$dt" > "$today" ]]; then
+         if [[ $n < 3 ]]; then
+            echo "$dt: $s"
+            let n=n+1
+         fi
+      fi
+  done
+
+exit 0
